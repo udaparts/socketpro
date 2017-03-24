c26a45f9cd7057858be656560f8d29ad1703add1
yye <support@udaparts.com>
2017-03-23 22:36:08 -0400

    code update

--- include/odbc/uodbc_server.h
+++ include/odbc/uodbc_server.h
@@ -17,13 +17,4 @@ extern "C" {
 
 typedef void (WINAPI *PSetOdbcDBGlobalConnectionString)(const wchar_t *dbConnection);
 
-namespace SPA {
-    namespace ServerSide {
-        namespace Odbc {
-
-        } //namespace Odbc
-    } //namespace ServerSide
-} //namespace SPA
-
-
 #endif
