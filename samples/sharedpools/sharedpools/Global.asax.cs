b77c3c19c6161660c0e02343ef356c23cd900944
SocketPro <support@udaparts.com>
2017-08-28 17:27:06 -0400

    add an empty web project

---
+++ samples/sharedpools/sharedpools/Global.asax.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using SocketProAdapter;
+using SocketProAdapter.ClientSide;
+using SocketProAdapter.UDB;
+
+namespace SharedPools
+{
+    public class Global : System.Web.HttpApplication
+    {
+        private static CSocketPool<CMysql> m_spUpdate = new CSocketPool<CMysql>();
+        private static CSocketPool<CMysql> m_spRead = new CSocketPool<CMysql>();
+        private static DataSet m_dsCache = new DataSet();
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+            m_spRead.ShutdownPool();
+            m_spUpdate.ShutdownPool();
+        }
+    }
+}
---
+++ samples/sharedpools/sharedpools/WebDisplay.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SocketProAdapter;
+using SocketProAdapter.ClientSide;
+using SocketProAdapter.UDB;
+
+
+namespace SharedPools
+{
+    public partial class WebDisplay : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
