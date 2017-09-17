df700a65b2fa940383ee3c9620d2d59106516934
yye <support@udaparts.com>
2017-09-16 22:05:05 -0400

    Merge branch 'master' into mmdb
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- include/mysql/umysql.h
+++ include/mysql/umysql.h
@@ -6,9 +6,9 @@
 
 namespace SPA {
     namespace Mysql {
-        static const unsigned int sidMysql = (unsigned int) sidReserved + 0x6FFFFFF1; //asynchronous mysql service id
+        static const unsigned int sidMysql = (unsigned int) sidReserved + 0x6FFFFFF1; //asynchronous MySQL/MariaDB service id
 
-        //error codes from async mysql server library
+        //error codes from async MySQL/MariaDB server library
         static const int ER_NO_DB_OPENED_YET = 1981;
         static const int ER_BAD_END_TRANSTACTION_PLAN = 1982;
         static const int ER_NO_PARAMETER_SPECIFIED = 1983;
@@ -19,6 +19,15 @@ namespace SPA {
         static const int ER_UNABLE_TO_SWITCH_TO_DATABASE = 1988;
         static const int ER_SERVICE_COMMAND_ERROR = 1989;
 
+        //The following defines are required by non-plugin version MySQL/MariaDB SQL-stream technology
+
+        static const int ER_MYSQL_LIBRARY_NOT_INITIALIZED = 1990;
+        /**
+         * Use MySQL/MariaDB embedded at SocketPro server side by default.
+         * Use this constance value for the input parameter flags with the method of CAsyncDBHandler::Open at client side
+         * to open a connection to remote MySQL/MariaDB server at SocketPro server instead of embedded MySQL/MariaDB
+         */
+        static const unsigned int USE_REMOTE_MYSQL = 0x1;
     } //namespace Mysql
 } //namespace SPA
 #endif
