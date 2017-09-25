648eca3d96653e0154ab7d7f7f1fb0ecd241acfb
SocketPro <support@udaparts.com>
2017-09-25 12:46:34 -0400

    create empty projects

---
+++ samples/stream_system/mid_tier/cplusplus/cplusplus.cpp
@@ -0,0 +1,9 @@
+
+
+#include "stdafx.h"
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
+
---
+++ samples/stream_system/mid_tier/cplusplus/stdafx.h
@@ -0,0 +1,16 @@
+
+#pragma once
+
+
+#include "../../../../include/mysql/umysql.h"
+
+#ifdef WIN32_64
+
+#include "targetver.h"
+#include <stdio.h>
+
+#else
+
+#endif
+
+#include <iostream>
