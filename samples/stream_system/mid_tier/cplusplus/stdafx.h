209ad1a9daf45284534b2ea52708f7e43540f669
yye <support@udaparts.com>
2017-10-08 22:41:21 -0400

    code update

--- samples/stream_system/mid_tier/cplusplus/stdafx.h
+++ samples/stream_system/mid_tier/cplusplus/stdafx.h
@@ -9,16 +9,6 @@ typedef SPA::ClientSide::CAsyncDBHandler<SPA::Mysql::sidMysql> CMySQLHandler;
 typedef SPA::CMasterPool<CMySQLHandler> CMySQLMasterPool;
 typedef CMySQLMasterPool::CSlavePool CMySQLSlavePool;
 
-//#include "../../../../include/async_sqlite.h"
-//typedef SPA::ClientSide::CSqlite CSqliteHandler;
-//typedef SPA::CMasterPool<CSqliteHandler> CSqliteMasterPool;
-//typedef CSqliteMasterPool::CSlavePool CSqliteSlavePool;
-
-//#include "../../../../include/async_odbc.h"
-//typedef SPA::ClientSide::COdbc COdbcHandler;
-//typedef SPA::CMasterPool<COdbcHandler> COdbcMasterPool;
-//typedef COdbcMasterPool::CSlavePool COdbcSlavePool;
-
 using namespace SPA::ServerSide;
 
 #ifdef WIN32_64
