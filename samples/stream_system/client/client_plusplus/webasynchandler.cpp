f1e43b1ace217d4506f39cf67156cc1f44fa3dc5
yye <support@udaparts.com>
2017-10-20 22:59:20 -0400

    code update

--- samples/stream_system/client/client_plusplus/webasynchandler.cpp
+++ samples/stream_system/client/client_plusplus/webasynchandler.cpp
@@ -7,32 +7,35 @@ CWebAsyncHandler::CWebAsyncHandler(CClientSocket *pClientSocket)
 }
 
 bool CWebAsyncHandler::QueryPaymentMaxMinAvgs(const wchar_t *filter, DMaxMinAvg mma, DCanceled canceled) {
-	return SendRequest(idQueryMaxMinAvgs, filter, [mma](CAsyncResult & ar) {
+	ResultHandler arh = [mma](CAsyncResult & ar) {
 		int res;
 		std::wstring errMsg;
 		CMaxMinAvg m_m_a;
 		ar >> res >> errMsg >> m_m_a;
 		if (mma)
 			mma(m_m_a, res, errMsg);
-	}, canceled, nullptr);
+	};
+	return SendRequest(idQueryMaxMinAvgs, filter, arh, canceled);
 }
 
 bool CWebAsyncHandler::GetMasterSlaveConnectedSessions(DConnectedSessions cs, DCanceled canceled) {
-	return SendRequest(idGetMasterSlaveConnectedSessions, [cs](CAsyncResult & ar) {
+	ResultHandler arh = [cs](CAsyncResult & ar) {
 		unsigned int master_connections, slave_conenctions;
 		ar >> master_connections >> slave_conenctions;
 		if (cs)
 			cs(master_connections, slave_conenctions);
-	}, canceled, nullptr);
+	};
+	return SendRequest(idGetMasterSlaveConnectedSessions, arh, canceled);
 }
 
 bool CWebAsyncHandler::UploadEmployees(const SPA::UDB::CDBVariantArray &vData, DUploadEmployees res, DCanceled canceled) {
-	return SendRequest(idUploadEmployees, vData, [res](CAsyncResult & ar) {
+	ResultHandler arh = [res](CAsyncResult & ar) {
 		int errCode;
 		std::wstring errMsg;
 		CInt64Array vId;
 		ar >> errCode >> errMsg >> vId;
 		if (res)
 			res(errCode, errMsg, vId);
-	}, canceled, nullptr);
+	};
+	return SendRequest(idUploadEmployees, vData, arh, canceled);
 }
