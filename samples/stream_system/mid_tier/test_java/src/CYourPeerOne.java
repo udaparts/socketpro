8cbb3cf46fb90156743f8b205be27e806cee6761
Yuancai Ye <support@udaparts.com>
2017-11-13 14:39:32 -0500

    code update

--- samples/stream_system/mid_tier/test_java/src/CYourPeerOne.java
+++ samples/stream_system/mid_tier/test_java/src/CYourPeerOne.java
@@ -1,6 +1,46 @@
 
-import SPA.ServerSide.*;
+import SPA.ServerSide.CCacheBasePeer;
+import SPA.ClientSide.CAsyncDBHandler;
+import SPA.ClientSide.CSqlite;
 
-public class CYourPeerOne extends CClientPeer {
+public class CYourPeerOne extends CCacheBasePeer {
+
+    @Override
+    protected CachedTableResult GetCachedTables(String defaultDb, int flags, boolean rowset, long index) {
+        CachedTableResult res = this.new CachedTableResult();
+        //CSqlMasterPool<CSqlite> Master = CYourServer.Master;
+        do {
+            if (!rowset) {
+                res.errMsg = "Client side doesn't ask for rowsets";
+                break;
+            }
+            CConfig config = CConfig.getConfig();
+            if (config.m_vFrontCachedTable.isEmpty()) {
+                break;
+            }
+            if ((flags & CAsyncDBHandler.ENABLE_TABLE_UPDATE_MESSAGES) == CAsyncDBHandler.ENABLE_TABLE_UPDATE_MESSAGES) {
+                if (!getPush().Subscribe(CAsyncDBHandler.CACHE_UPDATE_CHAT_GROUP_ID, CAsyncDBHandler.STREAMING_SQL_CHAT_GROUP_ID)) {
+                    res.errMsg = "Failed in subscribing for table events"; //warning message
+                }
+            }
+            String sql = "";
+            for (String s : config.m_vFrontCachedTable) {
+                if (sql.length() != 0) {
+                    sql += ";";
+                }
+                sql += "SELECT * FROM " + s;
+            }
+            CSqlite handler = CYourServer.Master.Lock(); //use Lock and Unlock to avoid SQL stream overlap on a session within a multi-thread environment
+            if (handler == null) {
+                res.res = -1;
+                res.errMsg = "No connection to a master database";
+                break;
+            }
+            res.ms = handler.getDBManagementSystem();
+
+        } while (false);
+
+        return res;
+    }
 
 }
--- src/jadapter/jspa/src/SPA/ServerSide/Odbc.java
+++ src/jadapter/jspa/src/SPA/ServerSide/Odbc.java
@@ -1,10 +1,10 @@
-
 package SPA.ServerSide;
 
 public final class Odbc {
+
     /**
      * Set ODBC global connection string.
-     * 
+     *
      * @param dbConnection a global or default string for ODBC connection string
      */
     public final static native void SetOdbcDBGlobalConnectionString(String dbConnection);
