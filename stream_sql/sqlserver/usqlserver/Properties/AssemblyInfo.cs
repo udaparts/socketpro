3b7a27507482f84ef30a3ef50e4d88c2a49375fc
yye <support@udaparts.com>
2017-11-29 22:04:42 -0500

    add usqlserver

---
+++ stream_sql/sqlserver/usqlserver/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with the SQLCLR assembly.
+[assembly: AssemblyTitle("sql-stream plugin for MS SQL Server")]
+[assembly: AssemblyDescription("sql streaming processing and responsing")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("UDAParts")]
+[assembly: AssemblyProduct("usqlserver")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: System.Resources.NeutralResourcesLanguageAttribute("en")]
