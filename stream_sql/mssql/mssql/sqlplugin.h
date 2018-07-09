83b4645de833a8be77b8caef443f26b3278b3eb2
unknown <support@udaparts.com>
2018-07-08 22:51:22 -0400

    code update

--- stream_sql/mssql/mssql/sqlplugin.h
+++ stream_sql/mssql/mssql/sqlplugin.h
@@ -9,10 +9,12 @@ public:
 	CSqlPlugin(int param = 0);
 	~CSqlPlugin();
 
+public:
+	virtual bool Run(unsigned int listeningPort, unsigned int maxBacklog = 32, bool v6 = false);
+
 protected:
 	virtual bool OnSettingServer(unsigned int listeningPort, unsigned int maxBacklog, bool v6);
 	virtual bool OnIsPermitted(USocket_Server_Handle h, const wchar_t* userId, const wchar_t *password, unsigned int serviceId);
-	virtual bool Run(unsigned int listeningPort, unsigned int maxBacklog = 32, bool v6 = false);
 
 private:
 	CSocketProService<COdbcImpl> m_odbc;
