b7334f75d7625acb0458541a34d8b22f23c9b563
Yuancai Ye <support@udaparts.com>
2018-08-07 16:27:24 -0400

    initial add server test code

---
+++ stream_sql/mariadb/test_smysql/nbproject/Makefile-Debug.mk
@@ -0,0 +1,96 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+AS=as
+
+# Macros
+CND_PLATFORM=GNU-Linux-x86
+CND_DLIB_EXT=so
+CND_CONF=Debug
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/_ext/932346631/aserverw.o \
+	${OBJECTDIR}/_ext/932346631/membuffer.o \
+	${OBJECTDIR}/test_smysql.o
+
+
+# C Compiler Flags
+CFLAGS=-std=c++11 -Wall
+
+# CC Compiler Flags
+CCFLAGS=-std=c++11 -Wall
+CXXFLAGS=-std=c++11 -Wall
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=-ldl -lpthread -lboost_system
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/test_smysql
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/test_smysql: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/test_smysql ${OBJECTFILES} ${LDLIBSOPTIONS} -lstdc++
+
+${OBJECTDIR}/_ext/932346631/aserverw.o: ../../../include/aserverw.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/932346631
+	${RM} "$@.d"
+	$(COMPILE.c) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/932346631/aserverw.o ../../../include/aserverw.cpp
+
+${OBJECTDIR}/_ext/932346631/membuffer.o: ../../../include/membuffer.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/932346631
+	${RM} "$@.d"
+	$(COMPILE.c) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/932346631/membuffer.o ../../../include/membuffer.cpp
+
+${OBJECTDIR}/test_smysql.o: test_smysql.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.c) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/test_smysql.o test_smysql.cpp
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/test_smysql
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
