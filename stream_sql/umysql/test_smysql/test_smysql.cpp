4d245cbaab57dd0b049bafb2f2461a0a7cd8cfaf
Yuancai Ye <support@udaparts.com>
2018-08-07 16:57:40 -0400

    remove test_smysql project and code update

--- stream_sql/umysql/test_smysql/test_smysql.cpp
+++ stream_sql/umysql/test_smysql/test_smysql.cpp
@@ -9,15 +9,25 @@ class CMySocketProServer : public SPA::ServerSide::CSocketProServer
 
 protected:
     virtual bool OnSettingServer(unsigned int listeningPort, unsigned int maxBacklog, bool v6) {
+        DoMySQLAuthentication = nullptr;
         m_h = SPA::ServerSide::CSocketProServer::DllManager::AddALibrary("smysql");
         if (m_h) {
             PSetMysqlDBGlobalConnectionString SetMysqlDBGlobalConnectionString = (PSetMysqlDBGlobalConnectionString) GetProcAddress(m_h, "SetMysqlDBGlobalConnectionString");
             SetMysqlDBGlobalConnectionString(L"host=localhost;uid=root;database=sakila;pwd=Smash123;port=3306;timeout=20", true);
+            DoMySQLAuthentication = (PDoMySQLAuthentication) GetProcAddress(m_h, "DoMySQLAuthentication");
         }
         return true;
     }
 
+    virtual bool OnIsPermitted(USocket_Server_Handle h, const wchar_t* userId, const wchar_t *password, unsigned int serviceId) {
+        if (DoMySQLAuthentication) {
+            return DoMySQLAuthentication(h, userId, password, serviceId, L"host=localhost;port=3306;timeout=30");
+        }
+        return false;
+    }
+
 private:
+    PDoMySQLAuthentication DoMySQLAuthentication;
     HINSTANCE m_h;
 };
 
