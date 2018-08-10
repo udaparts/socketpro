ec4e984f8091b66db45e8d19df2caedab29dc1e4
Yuancai Ye <support@udaparts.com>
2018-08-10 10:33:36 -0400

    code update

--- stream_sql/mariadb/smysql/smysql.cpp
+++ stream_sql/mariadb/smysql/smysql.cpp
@@ -1,6 +1,8 @@
 
 #include "streamingserver.h"
 
+#if 1
+//mariadb plugin declaration
 maria_declare_plugin(async_sql) {
     MYSQL_DAEMON_PLUGIN,
             &CSetGlobals::Globals.async_sql_plugin,
@@ -17,3 +19,24 @@ maria_declare_plugin(async_sql) {
             MariaDB_PLUGIN_MATURITY_STABLE /* maturity */
 }
 maria_declare_plugin_end;
+
+#else
+//mysql plugin declaration
+mysql_declare_plugin(async_sql) {
+    MYSQL_DAEMON_PLUGIN,
+            &CSetGlobals::Globals.async_sql_plugin,
+            "UDAParts_SQL_Streaming",
+            "UDAParts",
+            "Async SQL Real-time Streaming Processing",
+            PLUGIN_LICENSE_PROPRIETARY,
+            async_sql_plugin_init, /* Plugin Init */
+            async_sql_plugin_deinit, /* Plugin Deinit */
+            0x0100 /* 1.0 */,
+            nullptr, /* status variables */
+            nullptr, /* system variables */
+            nullptr, /* reserved */
+            0 /* flags */
+}
+mysql_declare_plugin_end;
+
+#endif
