740a4a2c9752ef271e8be666455a9f7750364010
unknown <support@udaparts.com>
2018-08-26 21:43:02 -0400

    code update

--- src/njadapter/test.js
+++ src/njadapter/test.js
@@ -1,6 +1,16 @@
 var SPA=require('njadapter.js');
 var p=SPA.CS.newPool(268435457);
 var cs = SPA.CS;
+p.StartPool(cs.newCC('localhost',20901,'root','Smash123'),1);
+var hw = p.Seek();
+hw.SendRequest(8194, SPA.newBuffer().SaveString('Jack').SaveString('Smith'), (reqId, buffer)=>{
+	console.log(buffer.LoadString());
+});
+hw.SendRequest(8195, SPA.newBuffer().SaveUInt(30000));
+hw.SendRequest(8194, SPA.newBuffer().SaveString('Hilary').SaveString('Clinton'), (reqId, buffer)=>{
+	console.log(buffer.LoadString());
+});
+
 p.setPoolEvent((spe)=>{if(spe!=15)console.log(spe);});
 p.setReturned((reqId, q)=>{
 	console.log(reqId);
@@ -9,16 +19,6 @@ p.setReturned((reqId, q)=>{
 p.setAllProcessed((reqId)=>{
 	console.log(reqId);
 });
-p.StartPool(cs.newCC('localhost',20901,'root','Smash123'),1);
-var hw = p.Seek();
-hw.getSvsId();
-hw.SendRequest(8194, 12345);
-
-hw.SendRequest(8194, SPA.newBuffer().SaveString('Jack').SaveString('Smith'), (reqId, buffer)=>{
-	console.log(buffer.LoadString());
-});
-
-
 var func = function(spe) {
 	console.log(spe);
 };
