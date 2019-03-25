85091d61fef91a06474861baa9d8a10e646ce5be
Yuancai (Charlie) Ye <support@udaparts.com>
2019-03-25 10:41:51 -0400

    code update

--- src/phpadapter/file.php
+++ src/phpadapter/file.php
@@ -1,12 +1,17 @@
 <?php
 
 $dir = 'C:\\cyedev\\commlib\\boost_1_55_0\\stage\\lib64\\';
-$webroot = 'C:\\inetpub\\wwwroot\\';
+$webroot = '/home/yye/sp_test/';
 
 try {
     $file = GetSpHandler('my_file');
-    //streaming files downloading and uploading
     do {
+        //force processing requests left in client queue untile all of them is completed
+        if (!$file->WaitAll()) {
+            echo 'Session closed <br/>';
+            break;
+        }
+        //streaming files downloading and uploading
         if (!$file->Download($webroot . 'spfile1.test', $dir . 'jvm.lib', function($em) {
                     echo 'File download result: ';
                     echo var_dump($em) . '<br/>';
@@ -20,8 +25,8 @@ try {
         echo 'Downloading jvm.lib<br/>';
 
         $em = $file->Upload($webroot . 'spfile1.test', $dir . 'jvm_copy.lib', true);
-        echo 'Uploading jvm.lib completed: ';
-        echo var_dump($em) . '<br/>';
+        echo 'Uploaded jvm.lib completed: ';
+        echo var_dump($em) . '<br/><br/>';
     } while (false);
     echo 'PHP script completed without exception';
 } catch (Exception $e) {
--- tutorials/php/remote_file.php
+++ tutorials/php/remote_file.php
@@ -7,47 +7,58 @@ $cb = function($em) {
 $ok = false;
 try {
     $file = GetSpHandler('my_file');
-    //streaming files downloading and uploading
     do {
         $dir = 'C:\\cyedev\\commlib\\boost_1_55_0\\stage\\lib64\\';
-        $webroot = 'C:\\inetpub\\wwwroot\\';
-
-        if (!$file->Download('spfile1.test', $dir . 'jvm.lib', $cb))
+        $webroot = '/home/yye/sp_test/';
+        //force processing requests left in client queue untile all of them is completed
+        if (!$file->WaitAll()) {
             break;
+        }
+        //streaming files downloading and uploading
+        if (!$file->Download($webroot . 'spfile1.test', $dir . 'jvm.lib', $cb)) {
+            break;
+        }
         echo 'Downloading jvm.lib started<br/>';
-        if (!$file->Download('spfile2.test', $dir . 'libboost_wave-vc110-mt-1_55.lib', $cb))
+        if (!$file->Download($webroot . 'spfile2.test', $dir . 'libboost_wave-vc110-mt-1_55.lib', $cb)) {
             break;
+        }
         echo 'Downloading libboost_wave-vc110-mt-1_55.lib started<br/>';
-        if (!$file->Download('spfile3.test', $dir . 'libboost_log_setup-vc110-mt-gd-1_55.lib', $cb))
+        if (!$file->Download($webroot . 'spfile3.test', $dir . 'libboost_log_setup-vc110-mt-gd-1_55.lib', $cb)) {
             break;
+        }
         echo 'Downloading libboost_log_setup-vc110-mt-gd-1_55.lib started<br/>';
-        if (!$file->Download('spfile4.test', $dir . 'libboost_graph-vc110-mt-gd-1_55.lib', $cb))
+        if (!$file->Download($webroot . 'spfile4.test', $dir . 'libboost_graph-vc110-mt-gd-1_55.lib', $cb)) {
             break;
+        }
         echo 'Downloading libboost_graph-vc110-mt-gd-1_55.lib started<br/>';
-        $em = $file->Download('spfile5.test', $dir . 'libboost_filesystem-vc110-mt-1_55.lib', true); //sync
-        echo 'Downloading libboost_filesystem-vc110-mt-1_55.lib completed: <br/>';
-        echo var_dump($em) . '<br/>';
+        $em = $file->Download($webroot . 'spfile5.test', $dir . 'libboost_filesystem-vc110-mt-1_55.lib', true); //sync
+        echo 'Downloaded libboost_filesystem-vc110-mt-1_55.lib completed: ';
+        echo var_dump($em) . '<br/><br/>';
 
-        if (!$file->Upload($webroot . 'spfile1.test', $dir . 'jvm_copy.lib', $cb))
+        if (!$file->Upload($webroot . 'spfile1.test', $dir . 'jvm_copy.lib', $cb)) {
             break;
-        echo 'Uploading jvm.lib stared<br/>';
-        if (!$file->Upload($webroot . 'spfile2.test', $dir . 'libboost_wave-vc110-mt-1_55_copy.lib', $cb))
+        }
+        echo 'Uploading jvm.lib started<br/>';
+        if (!$file->Upload($webroot . 'spfile2.test', $dir . 'libboost_wave-vc110-mt-1_55_copy.lib', $cb)) {
             break;
+        }
         echo 'Uploading libboost_wave-vc110-mt-1_55.lib started<br/>';
-        if (!$file->Upload($webroot . 'spfile3.test', $dir . 'libboost_log_setup-vc110-mt-gd-1_55_copy.lib', $cb))
+        if (!$file->Upload($webroot . 'spfile3.test', $dir . 'libboost_log_setup-vc110-mt-gd-1_55_copy.lib', $cb)) {
             break;
+        }
         echo 'Uploading libboost_log_setup-vc110-mt-gd-1_55.lib started<br/>';
-        if (!$file->Upload($webroot . 'spfile4.test', $dir . 'libboost_graph-vc110-mt-gd-1_55_copy.lib', $cb))
+        if (!$file->Upload($webroot . 'spfile4.test', $dir . 'libboost_graph-vc110-mt-gd-1_55_copy.lib', $cb)) {
             break;
+        }
         echo 'Uploading libboost_graph-vc110-mt-gd-1_55.lib started<br/>';
         $em = $file->Upload($webroot . 'spfile5.test', $dir . 'libboost_filesystem-vc110-mt-1_55_copy.lib', true); //sync
-        echo 'Uploading libboost_filesystem-vc110-mt-1_55.lib completed: ';
-        echo var_dump($em) . '<br/>';
+        echo 'Uploaded libboost_filesystem-vc110-mt-1_55.lib completed: ';
+        echo var_dump($em) . '<br/><br/>';
         $ok = true;
     } while (false);
-    if ($ok)
+    if ($ok) {
         echo 'PHP script completed without exception';
-    else {
+    } else {
         echo 'Session closed with error: ';
         echo var_dump($file->Socket->Error) . '<br/>';
     }
