e86bc63b9b1b56022afe9ad63f714998ae57bbdd
UDAParts <support@udaparts.com>
2019-09-16 15:21:31 -0400

    code update

--- dev/ucomm/include/commutil.h
+++ dev/ucomm/include/commutil.h
@@ -6,15 +6,15 @@
 #include <sstream>
 
 #ifdef WIN32_64
-#if _MSC_VER < 1900
+#if _MSC_VER < 1700
 #else
 #include <atomic>
-#define ATOMIC_FLAG_AVAILABLE
+#define ATOMIC_AVAILABLE
 #endif
 #include "wincommutil.h"
 #else
 #include <atomic>
-#define ATOMIC_FLAG_AVAILABLE
+#define ATOMIC_AVAILABLE
 #include "nixcommutil.h"
 #ifdef USE_BOOST_LARGE_INTEGER_FOR_DECIMAL
 #include <boost/multiprecision/cpp_int.hpp>
@@ -29,10 +29,10 @@ namespace SPA {
 
     class CSpinLock {
     private:
-#ifndef ATOMIC_FLAG_AVAILABLE
+#ifndef ATOMIC_AVAILABLE
         volatile long m_locked;
 #else
-        std::atomic_flag m_locked = ATOMIC_FLAG_INIT;
+        std::atomic<int> m_locked;
 #endif
 
         //no copy constructor
@@ -40,7 +40,7 @@ namespace SPA {
         //no assignment operator
         CSpinLock& operator=(const CSpinLock &sl);
 
-#if defined(MONITORING_SPIN_CONTENTION) && defined(ATOMIC_FLAG_AVAILABLE)
+#if defined(MONITORING_SPIN_CONTENTION) && defined(ATOMIC_AVAILABLE)
     public:
         std::atomic<UINT64> Contention;
 #endif
@@ -48,12 +48,10 @@ namespace SPA {
     public:
 
         CSpinLock()
-#ifdef ATOMIC_FLAG_AVAILABLE
-#ifdef MONITORING_SPIN_CONTENTION
-        : Contention(0)
-#endif
-#else
         : m_locked(0)
+#if defined(MONITORING_SPIN_CONTENTION) && defined(ATOMIC_AVAILABLE)
+        ,
+        Contention(0)
 #endif
         {
         }
@@ -67,17 +65,20 @@ namespace SPA {
          */
         inline UINT64 lock(UINT64 max_cycle = (~0)) volatile {
             UINT64 cycle = 0;
-#ifndef ATOMIC_FLAG_AVAILABLE
+#ifndef ATOMIC_AVAILABLE
             while (::_InterlockedCompareExchange(&m_locked, 1, 0)) {
 #else
-            while (m_locked.test_and_set(std::memory_order_acquire)) {
+            int no_lock = 0;
+            while (!m_locked.compare_exchange_weak(no_lock, 1, std::memory_order_acquire, std::memory_order_relaxed)) {
+                assert(no_lock);
+                no_lock = 0;
 #endif
                 ++cycle;
                 if (cycle >= max_cycle) {
                     break;
                 }
             }
-#if defined(MONITORING_SPIN_CONTENTION) && defined(ATOMIC_FLAG_AVAILABLE)
+#if defined(MONITORING_SPIN_CONTENTION) && defined(ATOMIC_AVAILABLE)
             if (cycle) {
                 Contention.fetch_add(cycle, std::memory_order_relaxed);
             }
@@ -98,10 +99,10 @@ namespace SPA {
          * @remark Must call the method lock first before calling this method
          */
         inline void unlock() volatile {
-#ifdef ATOMIC_FLAG_AVAILABLE
-            m_locked.clear(std::memory_order_release);
-#else
             assert(m_locked); //must call the method lock first
+#ifdef ATOMIC_AVAILABLE
+            m_locked.store(0, std::memory_order_release);
+#else
             m_locked = 0;
 #endif
         }
