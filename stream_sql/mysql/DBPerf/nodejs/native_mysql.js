562bf4ec5520cf3a01654743d59d8b5822fdf9ba
SocketPro <support@udaparts.com>
2019-10-18 10:43:13 -0400

    minor code update for MySQL native js perfermance study

--- stream_sql/mysql/DBPerf/nodejs/native_mysql.js
+++ stream_sql/mysql/DBPerf/nodejs/native_mysql.js
@@ -5,17 +5,18 @@ mysqlx.getSession({
     password: "Smash123"
 }).then((session)=> {
 	var row;
+	var stmt = 'SELECT * FROM sakila.actor WHERE actor_id between 11 and 20';
 	var start = new Date();
 	var count = 100000;
 	for (var n = 0; n < count; ++n) {
-		res = session.sql('SELECT * FROM sakila.actor WHERE actor_id between 11 and 20')
+		res = session.sql(stmt)
 		.execute()
 		.then((res)=>{
 			while (row = res.fetchOne()) {
 			}
 		});
 	}
-	res = session.sql('SELECT * FROM sakila.actor WHERE actor_id between 11 and 20')
+	res = session.sql(stmt)
 	.execute()
 	.then((res)=>{
 		while (row = res.fetchOne()) {
