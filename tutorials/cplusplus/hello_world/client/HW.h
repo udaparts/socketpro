c0af3bc0a621fcc6965ac6d950a0137efd7640df
unknown <support@udaparts.com>
2020-07-01 12:34:18 -0400

    code update

--- tutorials/cplusplus/hello_world/client/HW.h
+++ tutorials/cplusplus/hello_world/client/HW.h
@@ -1,5 +1,4 @@
 #include "../../../../include/aclientw.h"
-#include "../../uqueue_demo/mystruct.h"
 
 #ifndef ___SOCKETPRO_CLIENT_HANDLER_HW_H__
 #define ___SOCKETPRO_CLIENT_HANDLER_HW_H__
@@ -17,24 +16,6 @@ public:
 
 	HelloWorld(CClientSocket *pClientSocket)
 		: CAsyncServiceHandler(sidHelloWorld, pClientSocket) {
-	}
-
-public:
-
-	std::wstring SayHello(const wchar_t* firstName, const wchar_t* lastName) {
-		std::wstring SayHelloRtn;
-		bool bProcessRy = ProcessR1(idSayHelloHelloWorld, firstName, lastName, SayHelloRtn);
-		return SayHelloRtn;
-	}
-
-	void Sleep(int ms) {
-		bool bProcessRy = ProcessR0(idSleepHelloWorld, ms);
-	}
-
-	CMyStruct Echo(const CMyStruct& ms) {
-		CMyStruct EchoRtn;
-		bool bProcessRy = ProcessR1(idEchoHelloWorld, ms, EchoRtn);
-		return EchoRtn;
 	}
 };
 #endif
--- tutorials/cplusplus/hello_world/client/hw_client.cpp
+++ tutorials/cplusplus/hello_world/client/hw_client.cpp
@@ -1,6 +1,6 @@
-
 #include "stdafx.h"
 #include "HW.h"
+#include "../../uqueue_demo/mystruct.h"
 
 int main(int argc, char* argv[]) {
     typedef CSocketPool<HelloWorld, CClientSocket> CMyPool;
@@ -14,35 +14,34 @@ int main(int argc, char* argv[]) {
     CMyPool spHw;
     CMyStruct ms0;
     SetMyStruct(ms0);
-    //optionally start a persistent queue at client side for auto failure recovery and once-only delivery
-    //spHw.SetQueueName("helloworld");
-    bool ok = spHw.StartSocketPool(cc, 1, 1);
+
+    //spHw.SetQueueName("helloworld"); //optionally start a queue for auto failure recovery
+    bool ok = spHw.StartSocketPool(cc, 1, 1); //1
     if (!ok) {
         std::cout << "Failed in connecting to remote helloworld server" << std::endl;
         return -1;
     }
-    auto hw = spHw.Seek(); //or auto hw = spHw.Lock();
+    auto hw = spHw.Seek(); //2 or auto hw = spHw.Lock();
 
     //process requests one by one synchronously
-    std::wcout << hw->async<std::wstring>(idSayHelloHelloWorld, L"Jone", L"Dole").get() << std::endl;
+    std::wcout << hw->async<std::wstring>(idSayHelloHelloWorld, L"Jone", L"Dole").get() << std::endl; //3
     hw->async(idSleepHelloWorld, (unsigned int) 5000).get();
-    CMyStruct ms = hw->async<CMyStruct, CMyStruct>(idEchoHelloWorld, ms0).get();
+    CMyStruct ms = hw->async<CMyStruct, CMyStruct>(idEchoHelloWorld, ms0).get(); //4
     assert(ms == ms0);
 
     //asynchronously process multiple requests with inline batching for best network efficiency
-    ok = hw->SendRequest(idSayHelloHelloWorld, L"Jack", L"Smith", [](CAsyncResult & ar) {
+    ok = hw->SendRequest(idSayHelloHelloWorld, L"Jack", L"Smith", [](CAsyncResult & ar) { //5
         std::wstring ret;
         ar >> ret;
         std::wcout << ret << std::endl;
     });
     CMyStruct ms1;
     SetMyStruct(ms1);
-    ok = hw->SendRequest(idEchoHelloWorld, ms1, [&ms, &ms1](CAsyncResult & ar) {
+    ok = hw->SendRequest(idEchoHelloWorld, ms1, [&ms, &ms1](CAsyncResult & ar) { //6
         ar >> ms;
         assert(ms == ms1);
     });
-    ok = hw->SendRequest(idSleepHelloWorld, (int) 5000, NULL_RH);
-    ok = hw->WaitAll();
+    hw->async(idSleepHelloWorld, (unsigned int) 5000).get(); //7
     std::wcout << L"Press a key to shutdown the demo application ......" << std::endl;
     ::getchar();
     return 0;
