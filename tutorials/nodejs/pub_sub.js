a59a082dbd4a3ec03df72a1520d53bbdccfa8afe
unknown <support@udaparts.com>
2020-07-25 19:08:26 -0400

    code update

--- tutorials/nodejs/pub_sub.js
+++ tutorials/nodejs/pub_sub.js
@@ -1,32 +1,25 @@
 "use strict";
-
 //loading SocketPro adapter (nja.js + njadapter.node) for nodejs
 var SPA = require('nja.js');
-
 const sid = SPA.SID.sidReserved + 1; //hello world service id
 
 //hello world service supports the following three requests
 const idSayHello = SPA.BaseID.idReservedTwo + 1;
-const idSleep = idSayHello + 1;
-const idEcho = idSleep + 1;
+const idSleep = idSayHello + 1; const idEcho = idSleep + 1;
 
-var cs = SPA.CS; //CS == Client side
-cs.TLS.CA = 'ca.cert.pem'; //or 'root' on windows for server certificate authentication
-
-function onLineMessage() {
-    process.stdout.write('Online Message Push: ');
-    console.log(arguments);
-}
+var cs = SPA.CS; cs.TLS.CA = 'ca.cert.pem'; //1 'root' on windows for server certificate authentication
 
 //create a socket pool object
-var p = cs.newPool(sid);
-global.p = p;
+var p = cs.newPool(sid); global.p = p;
 
-//track various events if neccessary
-p.Push = onLineMessage;
+//track various message events if neccessary
+p.Push = function () { //2
+    process.stdout.write('Online Message Push: ');
+    console.log(arguments);
+};
 
 //create a secure connection context
-var cc = cs.newCC('localhost', 20901, 'root', 'Smash123', 0); //1 0 == NoEncryption, and 1 == TLSv1.x
+var cc = cs.newCC('localhost', 20901, 'root', 'Smash123', 0); //2 0 = NoEncryption or 1 = TLSv1.x
 
 //start a socket pool having one session to a remote server
 if (!p.Start(cc, 1)) {
@@ -34,9 +27,9 @@ if (!p.Start(cc, 1)) {
     return;
 }
 var hw = p.Seek(); //seek an async hello world handler
-var msg = hw.Socket.Push; //2
+var msg = hw.Socket.Push; //3
 
-//streaming all the following five requests and two messenger message requests
+//streaming all the following four requests and two messenger message requests
 var ok = hw.SendRequest(idSayHello, SPA.newBuffer().SaveString('Mary').SaveString('Smith'), q => {
     console.log(q.LoadString());
 });
@@ -48,17 +41,8 @@ ok = hw.SendRequest(idSleep, SPA.newBuffer().SaveInt(5000), q => {
 
 //prepare a real complex structure for a remote request
 var data = {
-    nullStr: null,
-    objNull: null,
-    aDate: new Date(),
-    aDouble: 1234.567,
-    aBool: true,
-    unicodeStr: 'Unicode',
-    asciiStr: 'ASCII',
-    objBool: true,
-    objString: 'test',
-    objArrString: ['Hello', 'world'],
-    objArrInt: [1, 76890]
+    nullStr: null, objNull: null, aDate: new Date(), aDouble: 1234.567, aBool: true, unicodeStr: 'Unicode',
+    asciiStr: 'ASCII', objBool: true, objString: 'test', objArrString: ['Hello', 'world'], objArrInt: [1, 76890]
 };
 console.log(data);
 
@@ -70,23 +54,14 @@ ok = hw.SendRequest(idEcho, SPA.newBuffer().Save(q => {
     q.SaveObject(data.objArrString).SaveObject(data.objArrInt);
 }), q => {
     //de-serialize once result comes from server
-    var d = {
-        nullStr: q.LoadString(),
-        objNull: q.LoadObject(),
-        aDate: q.LoadDate(),
-        aDouble: q.LoadDouble(),
-        aBool: q.LoadBool(),
-        unicodeStr: q.LoadString(),
-        asciiStr: q.LoadAString(),
-        objBool: q.LoadObject(),
-        objString: q.LoadObject(),
-        objArrString: q.LoadObject(),
-        objArrInt: q.LoadObject()
-    };
-    console.log(d);
+    console.log({
+        nullStr: q.LoadString(), objNull: q.LoadObject(), aDate: q.LoadDate(), aDouble: q.LoadDouble(),
+        aBool: q.LoadBool(), unicodeStr: q.LoadString(), asciiStr: q.LoadAString(), objBool: q.LoadObject(),
+        objString: q.LoadObject(), objArrString: q.LoadObject(), objArrInt: q.LoadObject()
+    });
 });
 
-async function asyncWait(hw, fName, lName) {
+(async (hw, fName, lName) => { //4
     try {
         //use sendRequest instead of SendRequest for Promise
         console.log(await hw.sendRequest(idSayHello, SPA.newBuffer().SaveString(fName).SaveString(lName), q => {
@@ -95,13 +70,10 @@ async function asyncWait(hw, fName, lName) {
     } catch (err) {
         console.log(err);
     }
-}
-
-//send a request by use of Promise, async and await
-asyncWait(hw, 'Hillary', 'Clinton');
+})(hw, 'Hillary', 'Clinton');
 
 //send a message to a user
-ok = msg.SendUserMessage('some_user_id', 'A test message from node.js'); //3
+ok = msg.SendUserMessage('some_user_id', 'A test message from node.js'); //5
 
 //send a message to three groups of connected clients
-ok = msg.Publish('A test publish message from node.js', [1, 3, 7]); //4
+ok = msg.Publish('A test publish message from node.js', [1, 3, 7]); //6
--- tutorials/nodejs/server_queue.js
+++ tutorials/nodejs/server_queue.js
@@ -1,7 +1,4 @@
-"use strict";
-
-//loading SocketPro adapter (nja.js + njadapter.node) for nodejs
-var SPA = require('nja.js');
+"use strict"; var SPA = require('nja.js');
 
 //define message request ids
 const idMessage0 = SPA.BaseID.idReservedTwo + 100;
@@ -9,21 +6,13 @@ const idMessage1 = SPA.BaseID.idReservedTwo + 101;
 const idMessage2 = SPA.BaseID.idReservedTwo + 102;
 const TEST_QUEUE_KEY = 'queue_name_0';
 
-var cs = SPA.CS; //CS == Client side
-
-//create a socket pool object
-var p = cs.newPool(SPA.SID.sidQueue);
-global.p = p;
-
-//create a connection context
-var cc = cs.newCC('localhost', 20901, 'root', 'Smash123');
+var cs = SPA.CS; var p = cs.newPool(SPA.SID.sidQueue); global.p = p;
 
 //start a socket pool having one session to a remote server
-if (!p.Start(cc, 1)) {
+if (!p.Start(cs.newCC('localhost', 20901, 'root', 'Smash123'), 1)) {
     console.log(p.Error);
     return;
-}
-var sq = p.Seek(); //seek an async persistent message queue handler
+} var sq = p.Seek(); //seek an async persistent message queue handler
 
 function testEnqueue(sq) {
     var idMsg, n, ok = true, buff = SPA.newBuffer(); const count = 1024;
@@ -31,12 +20,9 @@ function testEnqueue(sq) {
     for (n = 0; n < count; ++n) {
         var str = n + ' Object test';
         switch (n % 3) {
-            case 0:
-                idMsg = idMessage0; break;
-            case 1:
-                idMsg = idMessage1; break;
-            default:
-                idMsg = idMessage2; break;
+            case 0: idMsg = idMessage0; break;
+            case 1: idMsg = idMessage1; break;
+            default: idMsg = idMessage2; break;
         }
         ok = sq.Enqueue(TEST_QUEUE_KEY, idMsg, buff.SaveString('SampleName').SaveString(str).SaveInt(n)); //1
         if (!ok) break;
@@ -46,15 +32,12 @@ function testEnqueue(sq) {
 }
 sq.ResultReturned = (id, q) => { //2
     switch (id) {
-        case idMessage0:
-        case idMessage1:
-        case idMessage2:
+        case idMessage0: case idMessage1: case idMessage2:
             //parse a dequeued message which should be the same as the above enqueued message (two unicode strings and one int)
             var name = q.LoadString(), str = q.LoadString(), index = q.LoadInt(); //3
             //console.log('message id=' + id + ', name=' + name + ', str=' + str + ', index=' + index);
             return true; //true -- result has been processed
-        default:
-            break;
+        default: break;
     }
     return false;
 };
@@ -79,16 +62,11 @@ ok = testDequeue(sq); //8
         console.log('Going to call GetKeys and Flush without promises ......');
         sq.GetKeys((keys) => { console.log(keys); }); //10
         sq.Flush(TEST_QUEUE_KEY, (mc, fsize) => { //11
-            console.log({
-                msgs: mc,
-                fsize: fsize
-            });
+            console.log({ msgs: mc, fsize: fsize });
         });
-
         console.log('++++ use getKeys and flush instead of GetKeys and Flush, respectively with Promises ++++');
         var my_arr = await Promise.all([sq.getKeys(), sq.flush(TEST_QUEUE_KEY)]); //12
-        console.log(my_arr[0]);
-        console.log(my_arr[1]);
+        console.log(my_arr[0]); console.log(my_arr[1]);
     } catch (err) {
         console.log(err);
     }
