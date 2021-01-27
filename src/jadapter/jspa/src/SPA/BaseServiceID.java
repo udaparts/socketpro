8e7b0c226acf1d484c2e1c6a19b8654c3851b9ba
Yuancai (Charlie) Ye <support@udaparts.com>
2021-01-27 17:45:47 -0500

    code update

--- src/jadapter/jspa/src/SPA/BaseServiceID.java
+++ src/jadapter/jspa/src/SPA/BaseServiceID.java
@@ -8,6 +8,25 @@ public final class BaseServiceID {
     public static final int sidHTTP = (sidChat + 1);
     public static final int sidFile = (sidHTTP + 1);
     public static final int sidODBC = (sidFile + 1);
+
+    /**
+     * Your non-db service ids should be between (sidReserved + 1) and
+     * (sidDB_RESERVED - 1)
+     */
     public static final int sidReserved = 0x10000000;
     public static final int sidQueue = sidChat;
+
+    /**
+     * Your db streaming service ids must be between sidDB_RESERVED and
+     * (sidDB_UDAParts_RESERVED - 1)
+     */
+    public static final int sidDB_RESERVED = (sidReserved + 0x6FFFFF00);
+
+    public static final int sidDB_UDAParts_RESERVED = (sidReserved + 0x6FFFFFD0);
+
+    /**
+     * UDAParts reserves sidODBC and db streaming service ids from
+     * sidDB_UDAParts_RESERVED through sidDB_MAX
+     */
+    public static final int sidDB_MAX = (sidReserved + 0x6FFFFFFF);
 }
