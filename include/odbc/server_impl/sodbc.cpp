1471fb300b7df108c3f30d2a7799f612e166e7f1
Yuancai (Charlie) Ye <support@udaparts.com>
2021-02-02 13:13:35 -0500

    code update

--- include/odbc/server_impl/sodbc.cpp
+++ include/odbc/server_impl/sodbc.cpp
@@ -1,15 +1,6 @@
-
 #include "odbcimpl.h"
 
 std::shared_ptr<SPA::ServerSide::COdbcService> g_pOdbc;
-
-bool WINAPI DoODBCAuthentication(USocket_Server_Handle hSocket, const wchar_t *userId, const wchar_t *password, unsigned int nSvsId, const wchar_t *odbcDriver, const wchar_t *dsn) {
-    return SPA::ServerSide::COdbcImpl::DoSQLAuthentication(hSocket, userId, password, nSvsId, odbcDriver, dsn);
-}
-
-void WINAPI SetOdbcDBGlobalConnectionString(const wchar_t *dbConnection) {
-    SPA::ServerSide::COdbcImpl::SetGlobalConnectionString(dbConnection);
-}
 
 bool WINAPI InitServerLibrary(int param) {
     if (!SPA::ServerSide::COdbcImpl::SetODBCEnv(param))
--- include/odbc/uodbc_server.h
+++ include/odbc/uodbc_server.h
@@ -5,19 +5,4 @@
 #include "uodbc.h"
 #include "../spa_module.h"
 
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-    void U_MODULE_OPENED WINAPI SetOdbcDBGlobalConnectionString(const wchar_t *dbConnection);
-    bool U_MODULE_OPENED WINAPI DoODBCAuthentication(USocket_Server_Handle hSocket, const wchar_t *userId, const wchar_t *password, unsigned int nSvsId, const wchar_t *odbcDriver, const wchar_t *dsn);
-
-#ifdef __cplusplus
-}
-#endif
-
-typedef void (WINAPI *PSetOdbcDBGlobalConnectionString)(const wchar_t *dbConnection);
-typedef bool (WINAPI *PDoODBCAuthentication)(USocket_Server_Handle hSocket, const wchar_t *userId, const wchar_t *password, unsigned int nSvsId, const wchar_t *odbcDriver, const wchar_t *dsn);
-
 #endif
