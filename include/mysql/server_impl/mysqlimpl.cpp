49e8c1bac1997576f490e65b4bfff0ada7440e5c
Yuancai (Charlie) Ye <support@udaparts.com>
2021-02-06 10:21:29 -0500

    code update

--- include/mysql/server_impl/mysqlimpl.cpp
+++ include/mysql/server_impl/mysqlimpl.cpp
@@ -2724,7 +2724,15 @@ namespace SPA
 
         bool CMysqlImpl::DoSQLAuthentication(USocket_Server_Handle hSocket, const wchar_t *userId, const wchar_t *password, unsigned int nSvsId, const wchar_t * dbConnection) {
             CMysqlImpl impl;
-            CDBString db(dbConnection ? Utilities::ToUTF16(dbConnection) : u"host=localhost;port=3306;timeout=30");
+            CDBString db(dbConnection ? Utilities::ToUTF16(dbConnection) : u"");
+            if (!db.size()) {
+                m_csPeer.lock();
+                db = m_strGlobalConnection;
+                m_csPeer.unlock();
+            }
+            if (!db.size()) {
+                db = u"host=localhost;timeout=30";
+            }
             if (userId && ::wcslen(userId)) {
                 db += u";uid=";
                 db += Utilities::ToUTF16(userId);
