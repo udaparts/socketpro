63e64449bb9783e709c1ed6ac0131582ede482d7
Yuancai (Charlie) Ye <support@udaparts.com>
2021-04-17 22:06:38 -0400

    add myclienttests

---
+++ dev/ucomm/myclienttests/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace myclienttests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strConn = "Server=windesk;Database=sakila;user id=sa;password=Smash123;";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(strConn))
+                {
+                    conn.Open();
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                //display error message
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+        }
+    }
+}
