1287f3bd8d7aa1007f164e977a895dde047e73fa
Yuancai Ye <support@udaparts.com>
2021-04-20 16:58:53 -0400

    code update

--- dev/ucomm/myclienttests/Program.cs
+++ dev/ucomm/myclienttests/Program.cs
@@ -8,12 +8,29 @@ namespace myclienttests
     {
         static void Main(string[] args)
         {
-            string strConn = "Server=windesk;Database=sakila;user id=sa;password=Smash123;";
+            string strConn = "Server=windesk;Database=AdventureWorks;user id=sa;password=Smash123;Packet Size=2920";
             try
             {
                 using (SqlConnection conn = new SqlConnection(strConn))
                 {
                     conn.Open();
+                    string sql = "use sqltestdb;select employeeid,MyImAGE from employee where employeeid=1";
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    do
+                    {
+                        while (reader.Read())
+                        {
+
+                        }
+                        if (!reader.NextResult())
+                            break;
+                        while (reader.Read())
+                        {
+
+                        }
+                    } while (false);
+                    reader.Close();
                     conn.Close();
                 }
             }
