dc97ae11852ba34f166e1f1b487d372676b1e985
Yuancai (Charlie) Ye <support@udaparts.com>
2021-08-13 11:30:23 -0400

    add class CSqlServer into java adapter

---
+++ src/jadapter/jspa/src/SPA/ClientSide/CSqlServer.java
@@ -0,0 +1,24 @@
+package SPA.ClientSide;
+
+public class CSqlServer extends CAsyncDBHandler {
+
+    //asynchronous MS SQL service id
+    public final static int sidMsSql = SPA.BaseServiceID.sidReserved + 0x6FFFFFF2;
+
+    public final static int USE_QUERY_BATCHING = 0x10000000;
+    public final static int READ_ONLY = 0x20000000;
+    public final static int USE_ENCRYPTION = 0x40000000;
+
+    public CSqlServer() {
+        super(sidMsSql);
+    }
+
+    /**
+     * You may use the protected constructor when extending this class
+     *
+     * @param sid a service id
+     */
+    protected CSqlServer(int sid) {
+        super(sid);
+    }
+}
