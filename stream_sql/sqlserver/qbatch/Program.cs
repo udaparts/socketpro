1a58e7949879255e703090d485eb060698c48359
Yuancai (Charlie) Ye <support@udaparts.com>
2021-12-21 18:34:25 -0500

    code update

--- stream_sql/sqlserver/qbatch/Program.cs
+++ stream_sql/sqlserver/qbatch/Program.cs
@@ -29,12 +29,6 @@ class Program
         Console.WriteLine("res={0}, em:{1}", res, errMsg);
     };
 
-    static CSqlServer.DExecuteResult er = (handler, res, errMsg, affected, fail_ok, id) =>
-    {
-        Console.WriteLine("aff={0}, fails={1}, oks={2}, res={3}, em:{4}",
-            affected, (uint)(fail_ok >> 32), (uint)fail_ok, res, errMsg);
-    };
-
     static readonly DateTime DT1900 = new DateTime(1900, 1, 1, 0, 0, 0, DateTimeKind.Local);
 
     static void Main(string[] args)
@@ -54,41 +48,39 @@ class Program
             CSqlServer sql = spSql.Seek();
             bool ok = sql.Open("", dr); //use default database
             //creating DB mydevdb, two tables and a stored procedure
-            TestCreateTables(sql);
+            var lstT = TestCreateTables(sql);
             var ra = new List<KeyValuePair<CDBColumnInfoArray, CDBVariantArray>>();
-            CSqlServer.DRows r = (handler, rowData) =>
-            {
+            CSqlServer.DRows r = (handler, rowData) => {
                 //rowset data come here
                 int last = ra.Count - 1;
                 KeyValuePair<CDBColumnInfoArray, CDBVariantArray> item = ra[last];
                 item.Value.AddRange(rowData);
             };
 
-            CSqlServer.DRowsetHeader rh = (handler) =>
-            {
+            CSqlServer.DRowsetHeader rh = (handler) => {
                 //rowset header comes here
                 var item = new KeyValuePair<CDBColumnInfoArray, CDBVariantArray>(handler.ColumnInfo, new CDBVariantArray());
                 ra.Add(item);
             };
-            CDBVariantArray v = new CDBVariantArray() {
-                1, 23456.23m, DT1900,
+            var v = new CDBVariantArray() {
+                1, 23456.23, DT1900,
                 3, 1245.76m, DT1900,
                 0, 87678.75m, DT1900
             };
             //enable in-line query batching
             ok = sql.Open("", dr, DB_CONSTS.USE_QUERY_BATCHING);
-            List<Task<CSqlServer.SQLExeInfo>> vT = new List<Task<CAsyncDBHandler.SQLExeInfo>>();
+            var vT = new List<Task<CAsyncDBHandler.SQLExeInfo>>(lstT);
             try {
+                //parameterzied statements
                 vT.Add(sql.execute("delete from employee;delete from company"));
                 vT.Add(TestComapny(sql));
-                vT.Add(sql.execute("delete from company"));
-                vT.Add(TestComapnyEx(sql, r, rh));
-
                 vT.Add(TestEmployee(sql));
-                vT.Add(sql.execute("delete from employee"));
-                vT.Add(TestEmployeeEx(sql, r, rh));
-
                 vT.Add(TestStoredProc(sql, v, r, rh));
+
+                //session variables
+                vT.Add(sql.execute("delete from employee;delete from company"));
+                vT.Add(TestComapnyEx(sql, r, rh));
+                vT.Add(TestEmployeeEx(sql, r, rh));
             }
             catch (AggregateException ex) {
                 foreach (Exception e in ex.InnerExceptions) {
@@ -114,8 +106,10 @@ class Program
             Console.WriteLine("+++++ Start rowsets +++");
             foreach (KeyValuePair<CDBColumnInfoArray, CDBVariantArray> it in ra) {
                 Console.Write("Statement index = {0}", index);
-                if (it.Key.Count > 0)
-                    Console.WriteLine(", rowset with columns = {0}, records = {1}.", it.Key.Count, it.Value.Count / it.Key.Count);
+                if (it.Key.Count > 0) {
+                    Console.WriteLine(", rowset with columns = {0}, records = {1}.",
+                        it.Key.Count, it.Value.Count / it.Key.Count);
+                }
                 else
                     Console.WriteLine(", no rowset received.");
                 ++index;
@@ -129,7 +123,7 @@ class Program
 
     static Task<CSqlServer.SQLExeInfo> TestComapny(CSqlServer sql)
     {
-        bool ok = sql.Prepare("INSERT INTO company(ID,NAME,ADDRESS,Income)VALUES(?,?,?,?)", dr);
+        bool ok = sql.Prepare("INSERT INTO company VALUES(?,?,?,?)", dr);
         var vData = new CDBVariantArray() {
             //first set
             1, "Google Inc.",
@@ -147,7 +141,8 @@ class Program
         return sql.execute(vData);
     }
 
-    static Task<CSqlServer.SQLExeInfo> TestComapnyEx(CSqlServer sql, CSqlServer.DRows r, CSqlServer.DRowsetHeader rh)
+    static Task<CSqlServer.SQLExeInfo> TestComapnyEx(CSqlServer sql, CSqlServer.DRows r,
+        CSqlServer.DRowsetHeader rh)
     {
         var vData = new CDBVariantArray() {
             //first set
@@ -161,17 +156,16 @@ class Program
             "Apple Inc.",
             "1 Infinite Loop, Cupertino, CA 95014, USA"
         };
-        string stmt = "INSERT INTO company(ID,NAME,ADDRESS,Income)VALUES(?,?,?,?);select SYSDATETIME();" +
-        "INSERT INTO company(ID,NAME,ADDRESS,Income)VALUES(?,?,?,93600090060.12);select 1,2,3;" +
-        "INSERT INTO company(ID,NAME,ADDRESS,Income)OUTPUT Inserted.* VALUES(3,?,?,234000300070.14)";
+        string stmt = "INSERT INTO company VALUES(?,?,?,?);select SYSDATETIME();" +
+        "INSERT INTO company VALUES(?,?,?,93600090060.12);select 1,2,3;" +
+        "INSERT INTO company OUTPUT Inserted.* VALUES(3,?,?,234000300070.14)";
         return sql.execute(stmt, vData, r, rh);
     }
 
-    static Task<CSqlServer.SQLExeInfo> TestEmployee(CSqlServer sql)
-    {
+    static Task<CSqlServer.SQLExeInfo> TestEmployee(CSqlServer sql) {
         string wstr = m_wstr;
         string str = m_str;
-        string stmt = "insert into employee(CompanyId,name,JoinDate,myimage,DESCRIPTION,Salary)values(?,?,?,?,?,?)";
+        string stmt = "insert into employee values(?,?,?,?,?,?)";
         bool ok = sql.Prepare(stmt, dr);
         CDBVariantArray vData = new CDBVariantArray();
         using (CScopeUQueue sbBlob = new CScopeUQueue()) {
@@ -208,8 +202,8 @@ class Program
         }
     }
 
-    static Task<CSqlServer.SQLExeInfo> TestEmployeeEx(CSqlServer sql, CSqlServer.DRows r, CSqlServer.DRowsetHeader rh)
-    {
+    static Task<CSqlServer.SQLExeInfo> TestEmployeeEx(CSqlServer sql, CSqlServer.DRows r,
+        CSqlServer.DRowsetHeader rh) {
         string wstr = m_wstr;
         string str = m_str;
         CDBVariantArray vData = new CDBVariantArray();
@@ -246,15 +240,15 @@ class Program
         vData.Add(DT1900);
 
         var stmt = "insert into employee OUTPUT Inserted.* values(1,?,?,?,?,?);" +
-        "insert into employee(CompanyId,name,JoinDate,myimage,DESCRIPTION,Salary)values(1,?,?,?,?,?);select 245;" +
-        "insert into employee OUTPUT Inserted.* values(2,?,?,?,?,6254030.42);" +
+        "insert into employee values(1,?,?,?,?,?);" +
+        "insert into employee OUTPUT Inserted.* values(2,?,?,?,?,6254030.42);select 245;" +
         "exec sp_TestProc ?,? out,? out;exec sp_TestProc 0,? out,? out";
 
         return sql.execute(stmt, vData, r, rh);
     }
 
-    static Task<CSqlServer.SQLExeInfo> TestStoredProc(CSqlServer sql, CDBVariantArray vPData, CSqlServer.DRows r, CSqlServer.DRowsetHeader rh)
-    {
+    static Task<CSqlServer.SQLExeInfo> TestStoredProc(CSqlServer sql, CDBVariantArray vPData,
+        CSqlServer.DRows r, CSqlServer.DRowsetHeader rh) {
         CParameterInfo[] vInfo = { new CParameterInfo(), new CParameterInfo(), new CParameterInfo() };
         vInfo[0].DataType = tagVariantDataType.sdVT_I4;
 
@@ -271,29 +265,29 @@ class Program
         return sql.execute(vPData, r, rh);
     }
 
-    static void TestCreateTables(CSqlServer sql)
-    {
+    static List<Task<CSqlServer.SQLExeInfo>> TestCreateTables(CSqlServer sql) {
         string create_database = "use master;IF NOT EXISTS(SELECT * FROM sys.databases WHERE " +
             "name='mydevdb')BEGIN CREATE DATABASE mydevdb;END";
-        bool ok = sql.Execute(create_database, er);
+        var t0 = sql.execute(create_database);
         string use_database = "Use mydevdb";
-        ok = sql.Execute(use_database, er);
+        var t1 = sql.execute(use_database);
         string create_table = "IF NOT EXISTS(SELECT * FROM sys.tables WHERE name='company')" +
             "create table company(ID bigint PRIMARY KEY,name CHAR(64)NOT NULL,ADDRESS " +
             "varCHAR(256)not null,Income float not null)";
-        ok = sql.Execute(create_table, er);
+        var t2 = sql.execute(create_table);
         create_table = "IF NOT EXISTS(SELECT * FROM sys.tables WHERE name='employee')create" +
             " table employee(EMPLOYEEID bigint identity PRIMARY KEY,CompanyId bigint not null" +
             ",name CHAR(64)NOT NULL,JoinDate DATETIME2(3),MyIMAGE varbinary(max),DESCRIPTION" +
             " nvarchar(max),Salary decimal(15,2),FOREIGN KEY(CompanyId)REFERENCES company(id))";
-        ok = sql.Execute(create_table, er);
+        var t3 = sql.execute(create_table);
         string drop_proc = "IF EXISTS(SELECT * FROM sys.procedures WHERE name='sp_TestProc')" +
             "drop proc sp_TestProc";
-        ok = sql.Execute(drop_proc, er);
+        var t4 = sql.execute(drop_proc);
         string create_proc = "CREATE PROCEDURE sp_TestProc(@p_company_id int,@p_sum_salary " +
             "decimal(15,2)output,@p_last_dt datetime2(3) out)as select * from employee where" +
             " companyid>=@p_company_id;select @p_sum_salary=sum(salary)+@p_sum_salary from " +
             "employee where companyid>=@p_company_id;select @p_last_dt=SYSDATETIME()";
-        ok = sql.Execute(create_proc, er);
+        var t5 = sql.execute(create_proc);
+        return new List<Task<CSqlServer.SQLExeInfo>>() { t0, t1, t2, t3, t4, t5};
     }
 }
