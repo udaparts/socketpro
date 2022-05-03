45b0ab1dda7db147c7db9318684c693ee02b1cf7
Yuancai (Charlie) Ye <support@udaparts.com>
2022-05-03 16:21:59 -0400

    minor update

--- src/SproAdapter/Properties/AssemblyInfo35.cs
+++ src/SproAdapter/Properties/AssemblyInfo35.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.6.0.1")]
-[assembly: AssemblyFileVersion("4.6.0.1")]
+[assembly: AssemblyFileVersion("2.6.0.1")]
