010671165d2ff8686e69821bbcddaec35ce990cd
Yuancai (Charlie) Ye <support@udaparts.com>
2022-07-13 17:43:15 -0400

    code minor update

--- tutorials/glang/guione.go
+++ tutorials/glang/guione.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 )
 
 func main() {
-	fmt.Println("See test files at the directory tests")
+	if runtime.GOOS == "windows" {
+		fmt.Print("Make sure that you already copy the whole directory ../socketpro/bin/gspa into C:\\Program Files\\Go\\src")
+	} else {
+		fmt.Print("Make sure that you already copy the whole directory ../socketpro/bin/gspa into /usr/local/go/src")
+	}
+	fmt.Println(" before playing unit test files at the sub-directory tests")
 }
